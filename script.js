64dd5cfcda5052a41701f1e00458123bbfe361f8
ra7n1k <111421305+ra7n1k@users.noreply.github.com>
2025-10-27 14:38:18 +0000

    The repository has been uploaded.

---
+++ script.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("body form");
+const entries = document.querySelectorAll("body form input")
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let odds = [];
+    let total = 0;
+
+    entries.forEach((input, index) => {
+        odds.push(1 / (parseFloat(input.value) + 1))
+        total += odds[index];
+    });
+
+    entries.forEach((input, index) => {
+        odds[index] = Math.round(odds[index] / total * 10000) / 100;
+        input.parentNode.querySelector("output").textContent = "Odds: " + odds[index] + "%"
+    });
+});
